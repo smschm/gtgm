158261631e18ba858d956ebd491f1b16f34fdc77
S Schmitt <smschm@umich.edu>
2013-02-18 18:15:16 -0500

    some nondescript cleanup

--- game.lisp
+++ game.lisp
@@ -1,12 +1,15 @@
 (defpackage :gt-game
   (:use :common-lisp :s-xml-rpc)
   (:export :game-state :start-game :place-card :player-draw-card :score-game
-           :deck :discards :hands :trails))
+           :deck :discards :hands :expos))
 (in-package :gt-game)
 
 (defparameter +ranks+ (append '(0 0 0) (loop for i from 2 below 11 collect i)))
 (defparameter +suits+ 5)
 (defparameter +init-hand-size+ 8)
+
+(defun make-adj-vector-from (list)
+  (make-array (length list) :initial-contents list :adjustable t))
 
 (defun shuffle (list)
   (if (< (length list) 2) list
@@ -19,6 +22,14 @@
 	      (loop for rank in +ranks+
 		 collect (cons suit rank)))))
 
+(defun score-pile-v (pile)
+  (let ((penalty 0) (multiplier 1) (total 0))
+    (loop for c across pile do
+	 (progn (setf penalty -20)
+		(if (= (cdr c) 0) (incf multiplier)
+		    (incf total (cdr c)))))
+    (* multiplier (+ penalty total))))
+
 (defun score-pile (pile &key (penalized nil) (total 0) (multiplier 1))
   (if (null pile) (* (+ total (if penalized -20 0)) multiplier)
       (score-pile (cdr pile)
@@ -27,23 +38,49 @@
 		  :multiplier (if (= (cdar pile) 0)
 				  (+ multiplier 1) multiplier))))
 
+;(defclass game-state ()
+;  ((started :initform nil :accessor started)
+;   (hands :initform (make-array 2 :initial-contents
+;			(loop for i below 2 collect
+;			     (make-array 0 :fill-pointer 0)))
+;	  :accessor hands)
+;   (deck :initform (shuffle (make-fresh-deck)) :accessor deck)
+;   (discards :initform (make-array +suits+ :initial-contents
+;			   (loop for i below +suits+ collect
+;				(make-array 0 :fill-pointer 0)))
+;	     :accessor discards)
+;   (trails :initform (make-array 2 :initial-contents
+;			 (loop for i below 2 collect
+;			      (make-array +suits+ :initial-contents
+;				    (loop for j below +suits+ collect
+;					 (make-array 0 :fill-pointer 0)))))
+;	   :accessor trails)))
+
 (defclass game-state ()
   ((started :initform nil :accessor started)
-   (hands :initform '(nil nil) :accessor hands)
+   (hands :initform (make-array 2 :initial-element nil)
+	  :accessor hands)
    (deck :initform (shuffle (make-fresh-deck)) :accessor deck)
-   (discards :initform (make-array +suits+ :initial-element nil) :accessor discards)
-   (trails :initform (list (make-array +suits+ :initial-element nil)
-			   (make-array +suits+ :initial-element nil))
-	   :accessor trails)))
+   (discards :initform (make-array +suits+ :initial-element nil)
+	     :accessor discards)
+   (expos :initform (make-array '(2 5) :initial-element nil)
+	   :accessor expos)))
 
 (defmethod start-game ((g game-state))
   (setf (started g) t
-	(hands g) (list (subseq (deck g) 0 +init-hand-size+)
-			(subseq (deck g) +init-hand-size+ (* 2 +init-hand-size+)))
+;	(hands g) (make-array 2 :adjustable t :initial-contents
+;		     (loop for i below 2 collect
+;			  (make-adj-vector-from
+;			   (subseq (deck g) (* i +init-hand-size+)
+;				   (* (1+ i) +init-hand-size+)))))
+	(hands g) (make-array 2 :initial-contents
+		     (loop for i below 2 collect
+			  (subseq (deck g) (* i +init-hand-size+)
+				   (* (1+ i) +init-hand-size+))))
 	(deck g) (subseq (deck g) (* 2 +init-hand-size+))))
 
 (defmethod expedition-max ((g game-state) player suit)
-  (let ((expn (elt (elt (trails g) player) suit)))
+  (let ((expn (aref (expos g) player suit)))
     (if (null expn) 0
 	(cdar expn))))
 
@@ -51,22 +88,22 @@
   (let* ((player-hand (elt (hands g) player))
 	 (card (elt player-hand card-ix))
 	 (suit (car card))
-	 ;(rank (cdr card))
+	 (rank (cdr card))
 	 (remaining-cards (append (subseq player-hand 0 card-ix)
 				  (subseq player-hand (1+ card-ix))))
 	 (card-placed t)
 	 (status nil))
     (case where
-      ((:discard-pile)
+      ((:discard)
        (progn (setf (elt (discards g) suit)
 		    (cons card (elt (discards g) suit)))
 	      ; why doesn't push work here?
 	      (setf status t)))
-      ((:expedition)
-       (if (< (cdr card) (expedition-max g player suit))
-	   (progn (place-card g player card-ix 'discard-pile)
-		  (setf card-placed nil status 'discarded-instead))
-	   (progn (push card (elt (elt (trails g) player) suit))
+      ((:expo)
+       (if (< rank (expedition-max g player suit))
+	   (progn (place-card g player card-ix :discard)
+		  (setf card-placed nil status :discarded-instead))
+	   (progn (push card (aref (expos g) player suit))
 		  (setf status t))))
       (otherwise (setf card-placed nil)))
     (if card-placed (setf (elt (hands g) player) remaining-cards))
@@ -75,10 +112,10 @@
 (defmethod player-draw-card ((g game-state) player where &key pile-ix)
   (let ((status nil))
     (case where
-      ((:discard-pile)
+      ((:discard)
        (if (null (elt (discards g) pile-ix))
-	   (progn (player-draw-card g player 'deck)
-		  (setf status 'drew-from-deck))
+	   (progn (player-draw-card g player :deck)
+		  (setf status :drew-from-deck))
 	   (let ((card (pop (elt (discards g) pile-ix))))
 	     (push (elt (hands g) player) card)
 	     (setf status t))))
@@ -92,5 +129,5 @@
     status))
 
 (defmethod score-game ((g game-state))
-  (loop for tr in (trails g) collect
-       (loop for p across tr summing (score-pile p))))
+  (loop for i below 2 collect
+       (loop for j below +suits+ summing (score-pile (aref (expos g) i j)))))
--- supervisor.lisp
+++ supervisor.lisp
@@ -15,7 +15,7 @@
   `(progn (princ (quote ,@expr)) ,@expr))
 
 (defun card-to-struct (c) (if (null c) 0 (xml-rpc-struct :|rank| (cdr c) :|suit| (car c))))
-(defun cardlist-to-xml (cs) (mapcar #'card-to-struct cs))
+(defun cardlist-to-xml (cs) (map 'vector #'card-to-struct cs))
 (defun cardlistvector-to-xml (csv) (map 'vector #'cardlist-to-xml csv))
 
 (defun game-runner (player-uris)
@@ -38,16 +38,14 @@
     (player-call 1 "initialize" 0 0 ; opponent id = game id = 0 for now
                  1 (mapcar #'card-to-struct (elt (hands g) 1)))
     (block outer (loop do
-         (let* ((r0 (player-call turn "getPlay"
-                                 (cardlist-to-xml (elt (hands g) turn))
-                                 ;(map 'vector
-				 ;     (lambda (x) (and (null x) (car x)))
-				 ;     (discards g))
+         (let* ((r0 (player-call
+		     turn "getPlay"
+		     (cardlist-to-xml (elt (hands g) turn))
 				 (cardlistvector-to-xml (discards g))
                                  (cardlistvector-to-xml (elt (trails g) turn))
                                  (cardlistvector-to-xml
 				  (elt (trails g) (- 1 turn)))))
-                (card-ix (get-xml-rpc-struct-member r0 :|card_ix|)) ; TODO: replace with macro above
+                (card-ix (get-xml-rpc-struct-member r0 :|card_ix|))
                 (play-to (get-xml-rpc-struct-member r0 :|play_to|))
                 (draw-from (get-xml-rpc-struct-member r0 :|draw_from|))
                 (card-played (elt (elt (hands g) turn) card-ix)))
