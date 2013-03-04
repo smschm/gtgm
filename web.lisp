c9106dec2cdd0ffb91d99f094a8d3385b14668d3
s s <smschm@umich.edu>
2013-03-04 15:55:38 -0500

    web interface "done"

--- web.lisp
+++ web.lisp
@@ -10,10 +10,17 @@
 
 (defvar *path* (concatenate 'string (sb-posix:getenv "HOME") "/test.sql"))
 (defvar *db* (sqlite:connect *path*))
+(defvar *acceptor*)
 
 (eval-when (:compile-toplevel) (format t "compile-toplevel"))
 (eval-when (:load-toplevel) (format t "load-toplevel"))
 (eval-when (:execute) (format t "execute"))
+
+(defmacro sql-execute-single-or-nil (&body params)
+  `(handler-case (sqlite:execute-single *db* ,@params)
+     (sqlite:sqlite-error (c)
+       (declare (ignore c))
+       nil)))
 
 ;;;; login / dologin
 
@@ -28,11 +35,11 @@
 		 "select pwhash from users where name = ?;" name)))
     (and hash (equal hash (hash-password pw)))))
 
-(defun make-session-key (&key (len 32))
-    (let ((hexes "0123456789abcdef"))
+(defun make-session-key (&key (len 12))
+    (let ((hexes "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"))
       (map 'string #'identity
 	   (loop for i below len collect
-		(elt hexes (random 16))))))
+		(elt hexes (random (length hexes)))))))
 
 (defun /dologin (uname upw)
   (let ((successp (login-success-p uname upw))
@@ -61,8 +68,8 @@
 (defun session-verify ()
   (let* ((uname (hunchentoot:cookie-in "uname"))
 	 (session-key (hunchentoot:cookie-in "session"))
-	 (session-stored
-	  (sqlite:execute-single *db* "select session from users where name = ?;" uname)))
+	 (session-stored (sql-execute-single-or-nil 
+                       "select session from users where name = ?;" uname)))
     (if (and uname session-key session-stored (equal session-key session-stored))
 	uname nil)))
 
@@ -102,7 +109,11 @@
 	(progn (format t "npcactive: ~A~%" npcactive)
 	  (sqlite:execute-non-query *db*
 		"insert into npcs (owner, host, port, name, description, rating, active)
-                 values (?, ?, ?, ?, ?, ?, ?);" uname npchost npcport npcname npcdesc
+                 values (?, ?, ?, ?, ?, ?, ?);" uname npchost npcport
+                 ;(hunchentoot:escape-for-html npcname)
+                 ;(hunchentoot:escape-for-html npcdesc)
+                 (string-trim " " npcname)
+                 (string-trim " " npcdesc)
                  1000.0d0 (if (equal npcactive "yes") 1 0))
 	       (setf success t))
       (sqlite:sqlite-constraint-error (e)
@@ -146,13 +157,103 @@
 	   #p"html/npcedit.html" (list :valid nil :uname uname :id id)
 	  :stream s)))))
 
-;(defun /donpcedit (uname id newdesc newhost newport))
+(defun /donpcedit (uname id newdesc newhost newport)
+  (let* ((id-owner (sqlite:execute-single *db* "select owner from npcs where id = ?;" id))
+         (active-p (sqlite:execute-single *db* "select active from npcs where id = ?;" id))
+         (valid (and uname id id-owner active-p (equal id-owner uname))))
+    (if valid (sqlite:execute-non-query *db*
+                  "update npcs set host = ?, port = ?, description = ? where id = ?;"
+                  newhost newport (string-trim " " newdesc) id))
+    (with-output-to-string (s)
+      (html-template:fill-and-print-template
+       #p"html/donpcedit.html" `(:success ,valid)
+     :stream s))))
 
 (defun add-npcedit-to-handlers ()
   (hunchentoot:define-easy-handler (npcedit :uri "/npcedit") ((id :parameter-type 'integer))
     (setf (hunchentoot:content-type*) "text/html")
     (let ((uname (session-verify)))
-      (if uname (/npcedit uname id)))))
+      (if uname (/npcedit uname id))))
+  (hunchentoot:define-easy-handler (donpcedit :uri "/donpcedit")
+      ((id :parameter-type 'integer) newdesc newhost (newport :parameter-type 'integer))
+    (setf (hunchentoot:content-type*) "text/html")
+    (let ((uname (session-verify)))
+      (if uname (/donpcedit uname id newdesc newhost newport)))))
+;;;;
+
+(defun /leaderboard ()
+  (let ((results (sqlite:execute-to-list *db* "select * from npcs where active = 1 order by rating desc;")))
+    (with-output-to-string (s)
+      (html-template:fill-and-print-template
+       #p"html/leaderboard.html" `(:rows ,(loop for npc in results for rank from 1 collect
+                                               `(:rank ,rank :rating ,(seventh npc)
+                                                 :npcname ,(fifth npc)
+                                                 :owner ,(second npc)
+                                                 :npcdesc ,(sixth npc))))
+       :stream s))))
+
+(defun add-leaderboard-to-handlers ()
+  (hunchentoot:define-easy-handler (leaderboard :uri "/leaderboard") ()
+    (setf (hunchentoot:content-type*) "text/html")
+    (/leaderboard)))
+
+;;;;
+
+(defun /user (uname)
+  (let ((host (if uname
+                  (sqlite:execute-single *db* "select host from users where name = ?;" uname)
+                  nil)))
+    (with-output-to-string (s)
+      (html-template:fill-and-print-template
+       #p"html/user.html" `(:loggedin ,uname :uname ,uname :uhost ,host) :stream s))))
+
+
+
+(defun /douser (uname curpw newpw newpw2 host)
+  (let ((valid (and uname (equal (hash-password curpw)
+                                 (sql-execute-single-or-nil
+                                   "select pwhash from users where name = ?;" uname))))
+        (pw-updated nil))
+    (if valid
+        (progn (sqlite:execute-non-query *db*
+                  "update users set host = ? where name = ?;"
+                  host uname)
+               (if (and (not (equal newpw "")) (equal newpw newpw2))
+                   (progn (sqlite:execute-non-query *db*
+                             "update users set pwhash = ? where name = ?;"
+                             (hash-password newpw) uname)
+                          (setf pw-updated t)))))
+    (with-output-to-string (s)
+      (html-template:fill-and-print-template
+       #p"html/douser.html" `(:valid ,valid :pwupdated ,pw-updated) :stream s))))
+
+(defun add-user-to-handlers ()
+  (hunchentoot:define-easy-handler (user :uri "/user") ()
+    (setf (hunchentoot:content-type*) "text/html")
+    (let ((uname (session-verify)))
+      (/user uname)))
+  (hunchentoot:define-easy-handler (douser :uri "/douser") (curpw newpw newpw2 host)
+    (setf (hunchentoot:content-type*) "text/html")
+    (/douser (session-verify) curpw newpw newpw2 host)))
+
+;;;;
+
+(defun /npctoggle (uname id)
+  (let* ((id-owner (sqlite:execute-single *db* "select owner from npcs where id = ?;" id))
+         (active-p (sqlite:execute-single *db* "select active from npcs where id = ?;" id))
+         (valid (and uname id id-owner active-p (equal id-owner uname)))
+         (new-active (- 1 active-p)))
+    (if valid
+        (sqlite:execute-non-query *db* "update npcs set active = ? where id = ?;" new-active id))
+	(with-output-to-string (s)
+	  (html-template:fill-and-print-template
+	   #p"html/npctoggle.html"
+	   `(:valid ,valid :newactive ,(equal new-active 1)) :stream s))))
+
+(defun add-npctoggle-to-handlers ()
+  (hunchentoot:define-easy-handler (npctoggle :uri "/npctoggle") ((id :parameter-type 'integer))
+    (setf (hunchentoot:content-type*) "text/html")
+    (/npctoggle (session-verify) id)))
 
 ;;;;
 
@@ -164,6 +265,20 @@
 
 ;;;;
 
+(defun add-root-to-handlers ()
+  (hunchentoot:define-easy-handler (root :uri "/") ()
+    (setf (hunchentoot:content-type*) "text/html")
+    (let ((uname (session-verify)))
+      (with-output-to-string (s)
+        (html-template:fill-and-print-template
+         #p"html/index.html" `(:loggedin ,uname :uname ,uname)
+         :stream s)))))
+
+;;;;
+
 (defun h-start ()
-  (hunchentoot:start
-   (make-instance 'hunchentoot:easy-acceptor :port 4242)))
+  (setf *acceptor*
+        (hunchentoot:start
+         (make-instance 'hunchentoot:easy-acceptor :port 4242)))
+  (setf (hunchentoot:acceptor-error-template-directory *acceptor*) nil
+        (hunchentoot:acceptor-document-root *acceptor*) nil))
